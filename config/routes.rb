40a50ddea6a13b72305953a2dd1247d32abc423a
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-09-10 17:43:31 +0900

    検索機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'products#index'
+  get 'products/search'
 end
